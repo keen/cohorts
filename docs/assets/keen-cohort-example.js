88ce0186006cab3544432903f0afc469a8cc574a
dustinlarimer <dustinlarimer@gmail.com>
2016-10-17 15:23:18 -0700

    Update header and demo

--- docs/assets/keen-cohort-example.js
+++ docs/assets/keen-cohort-example.js
@@ -22,12 +22,23 @@
     ds.insertColumn(1, '3 weeks ago',  [ 55.1, 43.4, 41.7 ]);
     ds.insertColumn(1, '4 weeks ago',  [ 43.1, 38.2, 38.1, 29.1 ]);
     ds.insertColumn(1, '5 weeks ago',  [ 35.5, 33.1, 25.1, 23.4, 22.9 ]);
-    ds.insertColumn(1, '6 weeks ago',  [ 21.8, 20.1, 20.8, 19.8, 18.9, 18.4 ]);
-    ds.insertColumn(1, '7 weeks ago',  [ 17.8, 15.0, 14.8, 10.0, 8.9, 8.5, 7.1 ]);
-    ds.insertColumn(1, '8 weeks ago',  [ 15.2, 14.8, 14.3, 12.2, 10.9, 10.5, 10.1, 8.7 ]);
-    ds.insertColumn(1, '9 weeks ago',  [ 12.7, 12.1, 10.8, 10.5, 10.3, 9.5, 6.1, 5.8, 4.8 ]);
-    ds.insertColumn(1, '10 weeks ago', [ 10.3, 10.1, 10.0, 09.7, 9.2, 7.4, 6.2, 6.2, 3.1, 1.4 ]);
 
+    if (cohorts.length >= 6) {
+      ds.insertColumn(1, '6 weeks ago',  [ 21.8, 20.1, 20.8, 19.8, 18.9, 18.4 ]);
+    }
+
+    if (cohorts.length >= 7) {
+      ds.insertColumn(1, '7 weeks ago',  [ 17.8, 15.0, 14.8, 10.0, 8.9, 8.5, 7.1 ]);
+    }
+    if (cohorts.length >= 8) {
+      ds.insertColumn(1, '8 weeks ago',  [ 15.2, 14.8, 14.3, 12.2, 10.9, 10.5, 10.1, 8.7 ]);
+    }
+    if (cohorts.length >= 9) {
+      ds.insertColumn(1, '9 weeks ago',  [ 12.7, 12.1, 10.8, 10.5, 10.3, 9.5, 6.1, 5.8, 4.8 ]);
+    }
+    if (cohorts.length === 10) {
+      ds.insertColumn(1, '10 weeks ago', [ 10.3, 10.1, 10.0, 09.7, 9.2, 7.4, 6.2, 6.2, 3.1, 1.4 ]);
+    }
 
     setTimeout(function(){
       callback(ds);
